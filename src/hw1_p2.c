5f1777b918406f3d55eea9a24c9344b7fdf86b6a
Shin Hao,Fu <ne6121084@gs.ncku.edu.tw>
2023-10-02 21:51:16 +0800

    final

--- src/hw1_p2.c
+++ src/hw1_p2.c
@@ -1,6 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXN 100000
 
-int main(int argc, char* argv[]) {
-    printf("hello\n");
+
+int parent[MAXN];
+int height[MAXN];
+
+// Initialize the sets
+void initialize(int n) {
+    for (int i = 0; i < n; i++) {
+        parent[i] = -1; // Each element is the root of its own set with height -1
+        height[i] = 0;  // Initial height is 0
+    }
+}
+
+// Find the root of a set (with path compression)
+int find(int x) {
+
+    if (parent[x] < 0) {
+        return x; // x is the root of its own set
+    } else {
+        // Path compression: Update the parent to the root of the set
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+}
+
+// Perform a union operation using height rule
+void unionSets(int a, int b) {
+
+    int rootA = find(a);
+    int rootB = find(b);
+    // printf("%d %d", rootA,rootB);
+    if (rootA == rootB) {
+        return; // a and b are already in the same set
+    }
+    
+    // Attach the smaller tree (by height) to the root of the larger tree
+    if (height[rootA] < height[rootB]) {
+        parent[rootA] = rootB;
+    } else if (height[rootA] > height[rootB]) {
+        parent[rootB] = rootA;
+    } else {
+        // Heights are equal, attach either one and increase the height
+        parent[rootB] = rootA;
+        height[rootA]++;
+    }
+    return ;
+}
+
+// Check if two elements are in the same set
+int sameSet(int a, int b) {
+    return find(a) == find(b);
+}
+
+int main() {
+    int t;
+    
+    scanf("%d", &t);
+    // printf("There are(a) %d test case\n",t);
+    while (t--) {
+        int n, ops;
+        scanf("%d %d", &n, &ops);
+        // printf("There a %d node, and %d operation\n", n, ops);
+        initialize(n);
+
+        while (ops--) {
+            char op[10];
+            int a, b;
+            // printf("Enter operation\n");
+            scanf("%s", &op);
+        
+            if (strcmp(op,"union")==0) {
+                // Union operation
+                // printf("Union, enter two node : ");
+                scanf("%d %d", &a, &b);
+                unionSets(a, b);
+                // printf("Successfully union!\n");
+            } else if (strcmp(op,"find")==0) {
+                // Find operation
+                // printf("Find, Find root of node, enter node : ");
+                scanf("%d", &a);
+                // printf("the root of %d is %d\n", a,find(a));
+                printf("%d\n",find(a));
+
+            } else if (strcmp(op,"same")==0) {
+                // Same operation
+                // printf("Is Same, compare two node is at same tree, enter two node : ");
+                scanf("%d %d", &a, &b);
+                // printf("the anser is %s\n", sameSet(a, b) ? "true" : "false");
+                printf("%s\n", sameSet(a, b) ? "true" : "false");
+            }
+        }
+    }
+
     return 0;
 }
--- src/hw1_p3.c
+++ src/hw1_p3.c
@@ -1,6 +1,107 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-    printf("hello\n");
+// Structure to represent an edge in the graph
+struct Edge {
+    int source, target, weight;
+};
+
+// Structure to represent a disjoint-set (used for cycle detection)
+struct DisjointSet {
+    int *parent, *rank;
+    int size;
+};
+
+// Create a new disjoint-set of size V
+struct DisjointSet* createSet(int V) {
+    struct DisjointSet* set = (struct DisjointSet*)malloc(sizeof(struct DisjointSet));
+    set->size = V;
+    set->parent = (int*)malloc(V * sizeof(int));
+    set->rank = (int*)malloc(V * sizeof(int));
+
+    for (int i = 0; i < V; i++) {
+        set->parent[i] = i; // Each vertex is its own parent initially
+        set->rank[i] = 0;   // Rank is initially 0
+    }
+
+    return set;
+}
+
+// Find the set to which a vertex belongs (with path compression)
+int find(struct DisjointSet* set, int vertex) {
+    if (set->parent[vertex] != vertex)
+        set->parent[vertex] = find(set, set->parent[vertex]); // Path compression
+    return set->parent[vertex];
+}
+
+// Perform union of two sets (with union by rank)
+void unionSets(struct DisjointSet* set, int x, int y) {
+    int rootX = find(set, x);
+    int rootY = find(set, y);
+
+    if (rootX == rootY) return;
+
+    if (set->rank[rootX] < set->rank[rootY])
+        set->parent[rootX] = rootY;
+    else if (set->rank[rootX] > set->rank[rootY])
+        set->parent[rootY] = rootX;
+    else {
+        set->parent[rootY] = rootX;
+        set->rank[rootX]++;
+    }
+}
+
+// Compare function for sorting edges by weight in ascending order
+int compareEdges(const void* a, const void* b) {
+    return ((struct Edge*)a)->weight - ((struct Edge*)b)->weight;
+}
+
+// Kruskal's algorithm to find the minimum spanning tree
+long long kruskalMST(struct Edge* edges, int V, int E) {
+    //sort edge 
+    qsort(edges, E, sizeof(struct Edge), compareEdges);
+
+    struct DisjointSet* set = createSet(V);
+
+    long long minCost = 0;
+    int edgeCount = 0;
+
+    for (int i = 0; i < E; i++) {
+        int source = edges[i].source;
+        int target = edges[i].target;
+
+        if (find(set, source) != find(set, target)) {
+            minCost += (long long)edges[i].weight;
+            unionSets(set, source, target);
+            edgeCount++;
+        }
+
+        if (edgeCount == V - 1) // We have found V-1 edges
+            break;
+    }
+
+    free(set->parent);
+    free(set->rank);
+    free(set);
+
+    return minCost;
+}
+
+int main() {
+    int V, E;
+    // printf("Enter the vertex and edge number : ");
+    scanf("%d %d", &V, &E);
+
+    struct Edge* edges = (struct Edge*)malloc(E * sizeof(struct Edge));
+
+    for (int i = 0; i < E; i++) {
+        scanf("%d %d %d", &edges[i].source, &edges[i].target, &edges[i].weight);
+    }
+
+    long long minCost = kruskalMST(edges, V, E);
+    printf("%lld\n", minCost);
+
+    free(edges);
+
     return 0;
 }
